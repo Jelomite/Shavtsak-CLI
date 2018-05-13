22bc37db0aa4ac38ef39a4e35f26e7bc0be5bb25
Jelomite <moshesher1998@gmail.com>
2018-05-13 21:23:46 +0300

    added copy method

--- shavtsak.py
+++ shavtsak.py
@@ -319,6 +319,15 @@ class Shavtsak:
         self.assign([soldier if soldier is not soldier2 else soldier1 for soldier in soldiers2], day2, watch2)
         return None
 
+    def copy(self):
+        new = Shavtsak(self.soldiers)
+        new.days = self.days
+        new.watches = self.watches
+        new.schedule = self.schedule
+        new.reduced = .self.reduced
+        new.name = self.name
+        return new
+
     def __str__(self):
         from tabulate import tabulate
         # you know it's gonna be the real deal when you import tabulate
