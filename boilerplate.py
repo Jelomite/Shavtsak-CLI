563d8fe41f377818662064f2d13c64df256af596
Jelomite <moshesher1998@gmail.com>
2018-05-30 10:54:15 +0300

    simple boilerplate file to reduce typing

---
+++ boilerplate.py
@@ -0,0 +1,15 @@
+from shavtsak import Shavtsak, Soldier
+
+adam = Soldier('Adam', 2)
+sherman = Soldier('Sherman', 0)
+gil = Soldier('Gil', 0)
+karol = Soldier('Karol', 3)
+hadar = Soldier('Hadar', 1)
+mark = Soldier('Mark', 2)
+shahar = Soldier('Shahar', 2)
+noy = Soldier('Noy', 2)
+yahli = Soldier('Yahli', 0)
+luski = Soldier('Luski', 0)
+shemer = Soldier('Shemer', 2)
+
+soldiers = [adam, sherman, gil, karol, hadar, shahar, noy, yahli, luski, shemer]
